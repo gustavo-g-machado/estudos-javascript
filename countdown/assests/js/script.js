30f3e6507badc931f8efe1a030dd9f00cafb3382
gustavo-g-machado <155308009+gustavo-g-machado@users.noreply.github.com>
2024-02-25 20:37:43 -0300

    arrumando

---
+++ countdown/assests/js/script.js
@@ -0,0 +1,6 @@
+const dias = document.getElementById('dias');
+const horas = document.getElementById('horas');
+const minutos = documet.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const 
