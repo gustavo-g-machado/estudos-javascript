457665ac3b8c830ea1b5c36fbf52bd218932c76a
gustavo-g-machado <155308009+gustavo-g-machado@users.noreply.github.com>
2024-02-27 20:00:49 -0300

    bug fix

--- countdown/assests/js/script.js
+++ countdown/assests/js/script.js
@@ -1,6 +1,33 @@
-const dias = document.getElementById('dias');
-const horas = document.getElementById('horas');
-const minutos = documet.getElementById('minutos');
-const segundos = document.getElementById('segundos');
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
-const 
+const aPm = "16 Mar 2024";
+
+function countdown() {
+
+    const aPmDate = new Date(aPm);
+    const currentDate = new Date();
+
+    const totalSeconds = (aPmDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
