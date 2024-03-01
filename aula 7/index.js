2a5ea9a22be7a9e2f4935ea1945e7368ba6f59bb
gustavo-g-machado <155308009+gustavo-g-machado@users.noreply.github.com>
2024-03-01 14:37:53 -0300

    Create index.js

---
+++ aula 7/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas
+// COnstantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um numero
+// Não podem conter espaços ou traços
+// Case-sensitive
+// Nao pode modificar o valor de uma constante
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero  
+console.log(resultado);
+const resultDuplicado = resultado * 2;
+console.log(resultDuplicado);
