b82e0c860aab5b51bd8ea7668406c8b9c4ea64b4
gustavo-g-machado <155308009+gustavo-g-machado@users.noreply.github.com>
2024-03-01 13:26:48 -0300

    Create index.js

---
+++ aula 6/index.js
@@ -0,0 +1,20 @@
+let nome = 'João'; // Declaramos a variavel
+nome = 'Qualquer valor'; // Inicializando a variavel
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+
+/*
+console.log(nome, 'nasceu em 2002');
+console.log('Em 2019', nome, 'conheceu Luisa Sonza');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome, 'em 2015'); 
+*/
+
+// Não pode criar varíaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+
+let nomeClient = 'Gustavo';
+console.log(nomeClient);
+
+//Não pode começar o nome de uma variável com um numero, espaços ou traços
