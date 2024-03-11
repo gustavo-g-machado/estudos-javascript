70a5eb43fbfedfd8fa21399c45dba4c44071ef83
gustavo-g-machado <155308009+gustavo-g-machado@users.noreply.github.com>
2024-03-11 20:45:30 -0300

    ex01

---
+++ aula 11/ex01.js
@@ -0,0 +1,10 @@
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um segundo número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado foi: ${resultado}`);
+
+// Também pode ser feito assim -> alert(`O resultado foi: ${num1 + num2}`);
